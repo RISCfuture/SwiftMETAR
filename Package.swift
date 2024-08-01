a94bcb6afbea5e5662c30e2b812cb775e4515001
Tim Morgan <github@timothymorgan.info>
2024-07-31 17:51:54 -0700

    Package URL updates

--- Package.swift
+++ Package.swift
@@ -20,8 +20,8 @@ let package = Package(
         .package(url: "https://github.com/Quick/Nimble.git", from: "9.2.0"),
         .package(url: "https://github.com/objecthub/swift-numberkit.git", from: "2.4.2"),
         .package(url: "https://github.com/sharplet/Regex.git", from: "2.1.0"),
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
-        .package(url: "https://github.com/1024jp/GzipSwift", from: "6.0.0"),
+        .package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.0.0"),
+        .package(url: "https://github.com/1024jp/GzipSwift.git", from: "6.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
