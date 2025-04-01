15582bae1bcbbeaf7449ee78113d0c482ed0d4d3
Tim Morgan <github@timothymorgan.info>
2025-03-31 22:35:16 -0700

    Swift 6 dependency fixes

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         .package(url: "https://github.com/objecthub/swift-numberkit.git", from: "2.6.0"),
         .package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.4.3"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.5.0"),
-        .package(url: "https://github.com/Mr-Alirezaa/BuildableMacro.git", from: "0.5.0")
+        .package(url: "https://github.com/riscfuture/BuildableMacro.git", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
